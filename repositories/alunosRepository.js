1753bd267732dbe8f97e7b42ce4b3226c821c8b6
Lucas Gabriel Adelino Araújo <lucasgabrielz2208@gmail.com>
2025-08-01 21:29:29 -0300

    Estrutura JSON Inicial

---
+++ repositories/alunosRepository.js
@@ -0,0 +1,11 @@
+const Alunos = [
+    {
+        "nome" : "Lucas",
+        "nota1" : 10,
+        "nota2" : 10,
+        "nota3" : 10,
+        "nota3" : 10,
+        "nota5" : 10,
+        "frequencia" : 90,
+    }
+]
